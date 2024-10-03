9239f898f36c53c422fb389a54c9fdcc2133f43b
DasturchiQurbonboyev <dasturchiqurbonboyev@gmail.com>
2024-10-03 11:43:42 +0500

    input phone number

--- js/news.js
+++ js/news.js
@@ -10,3 +10,40 @@ buttons.forEach((button, index) => {
         img.classList.toggle('rotate');
     });
 });
+
+
+
+
+// --------- enroool  inputs------//
+
+
+function formatPhoneNumber(value) {
+    if (!value) return value;
+
+    const phoneNumber = value.replace(/[^\d]/g, '');
+    const phoneNumberLength = phoneNumber.length;
+
+    if (phoneNumberLength < 3) return phoneNumber;
+
+
+    let formattedNumber = `(${phoneNumber.slice(0, 2)}) `;
+    if (phoneNumberLength > 2) {
+        formattedNumber += phoneNumber.slice(2, 5);
+    }
+    if (phoneNumberLength > 5) {
+        formattedNumber += ` - ${phoneNumber.slice(5, 7)}`;
+    }
+    if (phoneNumberLength > 7) {
+        formattedNumber += ` - ${phoneNumber.slice(7, 9)}`;
+    }
+
+    return formattedNumber;
+}
+
+function phoneNumberFormatter() {
+    const inputField = document.getElementById('phoneInput');
+    const formattedInputValue = formatPhoneNumber(inputField.value);
+    inputField.value = formattedInputValue;
+}
+
+document.getElementById('phoneInput').addEventListener('input', phoneNumberFormatter);
